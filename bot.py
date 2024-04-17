10a025b690b23c05ffa3aa8a9e4e66715abe1014
Nikz <naybar.dev@gmail.com>
2024-04-16 23:11:46 -0300

    Added login to grass

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException, TimeoutException
 import csv
 import random  # Import the random module
 import sys
 import logging
+import pyautogui
 
 # Setup basic logging
 logging.basicConfig(filename='app.log', level=logging.WARNING, format='%(asctime)s %(levelname)s:%(message)s')
@@ -25,9 +25,22 @@ chrome_options.add_argument("--ignore-certificate-errors")  # Ignore certificate
 # Initialize the browser with the options
 driver = webdriver.Chrome(options=chrome_options)
 
+
+
 try:
     # Set the page load timeout to 5 seconds
     driver.set_page_load_timeout(5)
+
+    # Open the first URL in a new tab and navigate to the second URL
+    driver.execute_script("window.open('');")  # Open a new tab
+    driver.switch_to.window(driver.window_handles[1])  # Switch to the new tab
+    driver.get("https://app.getgrass.io/")  # Navigate to the URL
+
+    # Switch back to the first tab to continue processing the original URLs
+    driver.switch_to.window(driver.window_handles[0])
+    
+    #To press 'Control + 2'
+    pyautogui.hotkey('ctrl', '2')
 
     while True:  # Infinite loop to continuously process URLs
         # Load all URLs from CSV file into a list
